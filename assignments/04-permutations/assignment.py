608df89d6486a8c7a61fb7898a819bfaedf7c401
pyraxo <pyraxo@tfwno.gf>
2018-03-23 21:49:04 +0800

    Assignment 4

---
+++ assignments/04-permutations/assignment.py
@@ -0,0 +1,21 @@
+def gen_passcode(keyword):
+	"""
+	Generates a list of possible permutations of the characters in a string.
+    Args:
+        keyword: string
+            String of characters to generate permutations from
+
+    Returns:
+        A list, which contains all possible permutations
+    """
+
+    # Add your code below
+
+
+    return []
+
+if __name__ == "__main__":
+	print("The string is 'Kleen")
+	print("\n".join(gen_passcode("Kleen")))
+
+	# Add tests below
